c460c177bafb6238197ef6480b655f64de5e8a97
ace-suyash <ace.suyash07@gmail.com>
2026-06-16 02:45:22 +0530

    add message-controller

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,34 @@
+
+import * as messageService from '../services/message.service.js';
+
+export const getInbox = async (req, res, next) => {
+  try {
+    const conversations = await messageService.getInbox(req.user._id);
+    res.json({ success: true, conversations });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getConversation = async (req, res, next) => {
+  try {
+    const result = await messageService.getConversation(req.user._id, req.params.userId);
+    res.json({ success: true, ...result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const sendMessage = async (req, res, next) => {
+  try {
+    const { content } = req.body;
+    const message = await messageService.sendMessage({
+      senderId: req.user._id,
+      recipientId: req.params.userId,
+      content,
+    });
+    res.status(201).json({ success: true, message });
+  } catch (error) {
+    next(error);
+  }
+};
